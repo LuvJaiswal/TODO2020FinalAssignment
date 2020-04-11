4dc724c1d32fbb3f991dbf781b31d0dd86d6c80b
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-04-11 23:38:14 +0545

    MVVM model is set up finally

--- app/src/main/java/com/example/todo2020/HomeActivity.java
+++ app/src/main/java/com/example/todo2020/HomeActivity.java
@@ -1,14 +1,35 @@
 package com.example.todo2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update Recyclerview
+
+                Toast.makeText(HomeActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
