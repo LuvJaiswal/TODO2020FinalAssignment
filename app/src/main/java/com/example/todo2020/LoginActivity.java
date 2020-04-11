d3764f7d38ab030495e6b378403f0b5f44fc93df
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-04-11 16:38:10 +0545

    Sqlite Database Implemented for Login and Registration

--- app/src/main/java/com/example/todo2020/LoginActivity.java
+++ app/src/main/java/com/example/todo2020/LoginActivity.java
@@ -2,13 +2,53 @@ package com.example.todo2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginusername,loginPassword;
+    Button loginBtn;
+
+    DatabaseHelper db;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        db = new DatabaseHelper(this);
+
+
+        loginusername = (EditText)findViewById(R.id.login_username);
+        loginPassword = (EditText)findViewById(R.id.login_password);
+        loginBtn =(Button)findViewById(R.id.loginBtn);
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = loginusername.getText().toString().trim();
+                String pwd = loginPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user, pwd);
+                if(res == true)
+                {
+                    Intent HomePage = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(HomePage);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this,"Login Error",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/todo2020/RegisterActivity.java
+++ app/src/main/java/com/example/todo2020/RegisterActivity.java
@@ -2,14 +2,62 @@ package com.example.todo2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText mName,mPassword,memail,mConfirmpassword;
+    Button mRegisterbtn;
+    DatabaseHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        db = new DatabaseHelper(this);
+
+        mName =(EditText)findViewById(R.id.Reg_ET_name);
+        memail=(EditText)findViewById(R.id.Reg_ET_email);
+        mPassword=(EditText)findViewById(R.id.Reg_ET_password);
+        mConfirmpassword=(EditText)findViewById(R.id.Reg_ET_confirm_password);
+        mRegisterbtn=(Button) findViewById(R.id.Reg_createAcc_Btn);
+
+        mRegisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = mName.getText().toString().trim();
+                String pwd = mPassword.getText().toString().trim();
+                String cnf_pwd = mConfirmpassword.getText().toString().trim();
+
+                if(pwd.equals(cnf_pwd)){
+                    long val = db.addUser(user,pwd);
+                    if(val > 0){
+                        Toast.makeText(RegisterActivity.this,"You have registered",Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin = new Intent(RegisterActivity.this,LoginActivity.class);
+                        startActivity(moveToLogin);
+                    }
+                    else{
+                        Toast.makeText(RegisterActivity.this,"Registeration Error",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this,"Password is not matching",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
     }
 
 
