0fd81a20b7c005bd9996582482ee8aaacb5b462e
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-05-16 16:11:03 +0545

    Speech To Text implemented

--- app/src/main/java/com/example/todo2020/AddEditTodoActivityFragment.java
+++ app/src/main/java/com/example/todo2020/AddEditTodoActivityFragment.java
@@ -1,6 +1,7 @@
 package com.example.todo2020;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -9,9 +10,11 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,8 +24,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.NumberPicker;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 public class AddEditTodoActivityFragment extends Fragment {
 
@@ -40,11 +49,17 @@ public class AddEditTodoActivityFragment extends Fragment {
     public static final String EXTRA_PRIORITY =
             "com.example.todo2020.EXTRA_PRIORITY";
 
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
+    private static final int REQUEST_CODE_SPEECH_INPUT2 = 9000;
+
+
     private NoteViewModel noteViewModel;
 
     private EditText todoTitle, todoDescription;
     private NumberPicker numberPicker;
-    private Button button;
+
+    private ImageButton mVoicebutton;
+    private ImageButton mVoicebuttondes;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,14 +78,30 @@ public class AddEditTodoActivityFragment extends Fragment {
         Log.d(TAG, "Check the log here");
 
         todoTitle = (EditText) view.findViewById(R.id.todoTitle);
+        mVoicebutton = (ImageButton) view.findViewById(R.id.mic);
         todoDescription = (EditText) view.findViewById(R.id.todoDescription);
+        mVoicebuttondes = (ImageButton) view.findViewById(R.id.mic2);
+
         numberPicker = (NumberPicker) view.findViewById(R.id.np_picker);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(10);
 
+        mVoicebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+
+        mVoicebuttondes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakdescription();
+            }
+        });
 
 
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
 
 
         Bundle bundle = this.getArguments();
@@ -87,6 +118,7 @@ public class AddEditTodoActivityFragment extends Fragment {
 
             getActivity().setTitle("Add Todo");
         }
+
 
     }
 
@@ -108,7 +140,7 @@ public class AddEditTodoActivityFragment extends Fragment {
             todoTitle.requestFocus();
             return;
         }
-        if (description.trim().isEmpty()){
+        if (description.trim().isEmpty()) {
             todoDescription.setError("Description required,please enter and save");
             todoDescription.requestFocus();
             return;
@@ -170,6 +202,58 @@ public class AddEditTodoActivityFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //Speech
+    public void speak() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "HI speak something");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Speech for description
+    public void speakdescription() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "HI speak something");
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT2);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SPEECH_INPUT:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    todoTitle.setText(result.get(0));
+                }
+                break;
+
+            case REQUEST_CODE_SPEECH_INPUT2:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    todoDescription.setText(results.get(0));
+                }
+                break;
+        }
+
+
+    }
+
+
 
 
 
